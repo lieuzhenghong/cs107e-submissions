f9fe3f71ed7fa21df032805915a5c7fce6e695e0
Ang Wei Neng <weineng.a@gmail.com>
2020-12-18 23:11:14 +0800

    Completed lab 1 in typewriter style

--- src/apps/larson.s
+++ src/apps/larson.s
@@ -8,39 +8,53 @@
  * Make sure to use GPIO pins 20-23 (or 20-27) for your scanner.
  */
 
-.equ DELAY, 0x3F0000
+.equ DELAY, 0x1F0000
 
-// configure GPIO 20 for output
+// FSEL all GPIO pins from 20-23 to set them to output
+// We do this by setting r1 to 000 000 000 000 001 001 001 001
 ldr r0, FSEL2
-mov r1, #1
+// Because mov can't handle too large constants, 
+// we have to add byte by byte (hence the 512)
+mov r1, #73
+orr r1, r1, #512
 str r1, [r0]
 
-// set bit 20
-mov r1, #(1<<20)
+setr2:
+    mov r3, #20
 
 loop: 
+    // We set the r3-th bit of SET0
+    mov r1, #1
+    mov r1, r1, lsl r3
 
-// set GPIO 20 high
-ldr r0, SET0
-str r1, [r0] 
+    // set SET0 to hold the value in r3
+    ldr r0, SET0
+    str r1, [r0] 
 
-// delay
-mov r2, #DELAY
-wait1:
-    subs r2, #1
-    bne wait1
+    // delay
+    mov r2, #DELAY
+    wait1:
+        subs r2, #1
+        bne wait1
 
-// set GPIO 20 low
-ldr r0, CLR0
-str r1, [r0] 
+    // set SET0 to hold the value in r3
+    ldr r0, CLR0
+    str r1, [r0] 
 
-// delay
-mov r2, #DELAY
-wait2:
-    subs r2, #1
-    bne wait2
+    // delay
+    mov r2, #DELAY
+    wait2:
+        subs r2, #1
+        bne wait2
 
-b loop
+    // We subtract 23 from r3
+    // and if it eq 0 (we have reached pin 23)
+    // we start again from pin 20 (see setr2)
+    subs r4, r3, #23
+    beq setr2
+
+    add r3, #1
+    b loop
 
 FSEL0: .word 0x20200000
 FSEL1: .word 0x20200004
