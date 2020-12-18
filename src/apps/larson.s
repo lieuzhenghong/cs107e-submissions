ae60344e89799100326071034f715a8c5feb5ca4
Ang Wei Neng <weineng.a@gmail.com>
2020-12-19 00:12:13 +0800

    Complete lab 1

--- src/apps/larson.s
+++ src/apps/larson.s
@@ -8,28 +8,57 @@
  * Make sure to use GPIO pins 20-23 (or 20-27) for your scanner.
  */
 
-.equ DELAY, 0x1F0000
+.equ DELAY, 0x090000
+.equ LOWGPIO, 20
+.equ HIGHGPIO, 23
 
 // FSEL all GPIO pins from 20-23 to set them to output
 // We do this by setting r1 to 000 000 000 000 001 001 001 001
 ldr r0, FSEL2
-// Because mov can't handle too large constants, 
-// we have to add byte by byte (hence the 512)
-mov r1, #73
-orr r1, r1, #512
+mov r2, #HIGHGPIO
+sub r2, #LOWGPIO
+
+mov r1, #0
+setled:
+    // multiplies by 3:
+    // bitshift (multiplys by 2)
+    // then plus r2
+    mov r3, r2, lsl #1
+    add r3, r2
+    
+    // The idea here is to do
+    // mov r4, (1 << r3)
+    // where r3 is 9, 6, 3, 0 etc
+    // So r4 is now 1, 1000, 1000000 etc.
+    // Then use orr to add r4 bytewise to r1
+    mov r5, #1
+    mov r4, r5, lsl r3
+    orr r1, r1, r4
+
+    // We decrement the loop and exit once r2 == -1
+    subs r2, #1
+    bge setled
+
 str r1, [r0]
 
-setr2:
-    mov r3, #20
+// R5 is direction register
+// if R5 is 1, we go forwards
+// if R5 is 0, we go backward
+mov r5, #0
+mov r3, #LOWGPIO
 
-loop: 
+// flipbit function flips R5 from 0 to 1 and vice versa
+flipbit:
+    eor r5, #1
+
+loop:
     // We set the r3-th bit of SET0
     mov r1, #1
     mov r1, r1, lsl r3
 
     // set SET0 to hold the value in r3
     ldr r0, SET0
-    str r1, [r0] 
+    str r1, [r0]
 
     // delay
     mov r2, #DELAY
@@ -39,7 +68,7 @@ loop:
 
     // set SET0 to hold the value in r3
     ldr r0, CLR0
-    str r1, [r0] 
+    str r1, [r0]
 
     // delay
     mov r2, #DELAY
@@ -47,13 +76,29 @@ loop:
         subs r2, #1
         bne wait2
 
-    // We subtract 23 from r3
-    // and if it eq 0 (we have reached pin 23)
-    // we start again from pin 20 (see setr2)
-    subs r4, r3, #23
-    beq setr2
+    // As mentioned, if R5 is 1 we go forwards
+    // otherwise we go backwards
 
-    add r3, #1
+    cmp r5, #1
+    beq forward
+
+    backward:
+        sub r3, #1
+        b next
+
+    forward:
+        add r3, #1
+        b next
+
+    next:
+        // At next, we check if we've hit the ends
+        // If we've hit either LOWGPIO or HIGHGPIO
+        // we flip the direction
+        cmp r3, #HIGHGPIO
+        beq flipbit
+        cmp r3, #LOWGPIO
+        beq flipbit
+
     b loop
 
 FSEL0: .word 0x20200000
