e1a22c3673f95b16c63d7537e9c862d01779253c
Ang Wei Neng <weineng.a@gmail.com>
2020-12-19 10:51:23 +0800

    Clean up code

--- src/apps/larson.s
+++ src/apps/larson.s
@@ -15,40 +15,41 @@
 
 // FSEL all GPIO pins from 20-23 to set them to output
 // We do this by setting r1 to 000 000 000 000 001 001 001 001
-ldr r0, FSEL2
-mov r2, #HIGHGPIO
-sub r2, #LOWGPIO
+setup:
+    ldr r0, FSEL2
+    mov r2, #HIGHGPIO
+    sub r2, #LOWGPIO
 
-mov r1, #0
-setled:
-    // multiplies by 3:
-    // bitshift (multiplies by 2)
-    // then plus r2
-    mov r3, r2, lsl #1
-    add r3, r2
-    
-    // The idea here is to do
-    // mov r4, (1 << r3)
-    // where r3 is 9, 6, 3, 0 etc
-    // So r4 is now 1, 1000, 1000000 etc.
-    // Then use orr to add r4 bytewise to r1
-    mov r5, #1
-    mov r4, r5, lsl r3
-    orr r1, r1, r4
+    mov r1, #0
+    setled:
+        // multiplies by 3:
+        // bitshift (multiplies by 2)
+        // then plus r2
+        mov r3, r2, lsl #1
+        add r3, r2
+        
+        // The idea here is to do
+        // mov r4, (1 << r3)
+        // where r3 is 9, 6, 3, 0 etc
+        // So r4 is now 1, 1000, 1000000 etc.
+        // Then use orr to add r4 bytewise to r1
+        mov r5, #1
+        mov r4, r5, lsl r3
+        orr r1, r1, r4
 
-    // We decrement the loop and exit once r2 < 0
-    subs r2, #1
-    bge setled
+        // We decrement the loop and exit once r2 < 0
+        subs r2, #1
+        bge setled
 
-str r1, [r0]
+    str r1, [r0]
 
-// R5 is direction register
-// if R5 is 1, we go forwards
-// if R5 is 0, we go backward
-mov r5, #0
+    // R5 is direction register
+    // if R5 is 1, we go forwards
+    // if R5 is 0, we go backward
+    mov r5, #0
 
-// R3 is the GPIO of the center LED
-mov r3, #LOWGPIO
+    // R3 is the GPIO of the center LED
+    mov r3, #LOWGPIO
 
 // flipbit function flips R5 between 0 and 1
 flipbit:
@@ -62,11 +63,11 @@ loop:
         // gpio in r3 will be set to brightest
         bl brightness_max
 
-        @ left gpio
+        @ @ left gpio
         sub r3, #1
         bl brightness_low
 
-        @ right gpio
+        @ @ right gpio
         add r3, #2
         bl brightness_low
 
@@ -100,24 +101,24 @@ loop:
     b loop
 
 
-// flow of code:
-// lightup -> brightness_max -> flick -> brightness_mid -> flick -> after_all_toggles (main code)
-lightup:
-    // We set the r3-th bit of SET0
-    mov r1, #1
-    mov r1, r1, lsl r3
-
-    mov r10, #0 // variable to see which cond to run
-    b brightness_max
-
-
 # Invariant
 # The whole thing runs DELAY times
 # Number of cycles on = Small Delay On 
 # Number of cycles off = Small Delay Off
 brightness_max:
     mov r6, #250
+    b flick
 
+brightness_mid:
+    mov r6, #50
+    b flick
+
+
+brightness_low:
+    mov r6, #5
+    b flick
+
+flick:
     mov r1, #1
     mov r1, r1, lsl r3
     ldr r0, SET0
@@ -142,59 +143,6 @@ brightness_max:
     
     bx lr
 
-brightness_mid:
-    mov r6, #50
-
-    mov r1, #1
-    mov r1, r1, lsl r3
-    ldr r0, SET0
-    str r1, [r0]
-
-    sub r2, r6
-    small_delay3:
-        subs r6, #1
-        bge small_delay3
-
-    // set CLR0 to hold the value in r3
-    ldr r0, CLR0
-    str r1, [r0]
-
-    mov r8, #SMALLDELAY
-    sub r7, r8, r6
-
-    sub r2, r7
-    small_delay4:
-        subs r7, #1
-        bge small_delay4
-    
-    bx lr
-
-brightness_low:
-    mov r6, #5
-
-    mov r1, #1
-    mov r1, r1, lsl r3
-    ldr r0, SET0
-    str r1, [r0]
-
-    sub r2, r6
-    small_delay5:
-        subs r6, #1
-        bge small_delay5
-
-    // set CLR0 to hold the value in r3
-    ldr r0, CLR0
-    str r1, [r0]
-
-    mov r8, #SMALLDELAY
-    sub r7, r8, r6
-
-    sub r2, r7
-    small_delay6:
-        subs r7, #1
-        bge small_delay6
-    
-    bx lr
 
 
 FSEL0: .word 0x20200000
